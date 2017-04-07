39bf92f048894779863bc79c3d5ddc202230682d
salocin0430 <nicolasruiz0430@gmail.com>
2017-04-07 11:54:22 -0500

    Acualizacion add reset RF

--- RF.vhd
+++ RF.vhd
@@ -7,6 +7,7 @@ entity RF is
     Port ( Rs1 : in  STD_LOGIC_VECTOR (4 downto 0);
            Rs2 : in  STD_LOGIC_VECTOR (4 downto 0);
            Rd : in  STD_LOGIC_VECTOR (4 downto 0);
+			  Reset : in  STD_LOGIC;
            DwR : in  STD_LOGIC_VECTOR (31 downto 0);
            Crs1 : out  STD_LOGIC_VECTOR (31 downto 0);
            Crs2 : out  STD_LOGIC_VECTOR (31 downto 0));
@@ -20,11 +21,19 @@ begin
 	process(Rs1,Rs2,Rd,DwR,Registers)
 	begin
 	Registers (0) <= x"00000000";
-	Crs1 <= Registers(conv_integer(Rs1));
-	Crs2 <= Registers(conv_integer(Rs2));
-	if(Rd /= "00000")then
-		Registers(conv_integer(Rd)) <= DwR;
-	end if;
+	
+	if (Reset = '1') then 
+			Registers <=(others => x"00000000");
+			Crs1 <= (others => '0');
+			Crs2 <= (others => '0');
+
+	else		
+			Crs1 <= Registers(conv_integer(Rs1));
+			Crs2 <= Registers(conv_integer(Rs2));
+			if(Rd /= "00000")then
+				Registers(conv_integer(Rd)) <= DwR;
+			end if;
+	end if;		
 	end process;
 
 end Behavioral;
---
+++ Tb_PCS2.vhd
@@ -0,0 +1,63 @@
+
+LIBRARY ieee;
+USE ieee.std_logic_1164.ALL;
+ 
+
+ 
+ENTITY Tb_PCS2 IS
+END Tb_PCS2;
+ 
+ARCHITECTURE behavior OF Tb_PCS2 IS 
+ 
+    -- Component Declaration for the Unit Under Test (UUT)
+ 
+    COMPONENT PCS2
+    PORT(
+         Clk : IN  std_logic;
+         Reset : IN  std_logic;
+         Result : OUT  std_logic_vector(31 downto 0)
+        );
+    END COMPONENT;
+    
+
+   --Inputs
+   signal Clk : std_logic := '0';
+   signal Reset : std_logic := '0';
+
+ 	--Outputs
+   signal Result : std_logic_vector(31 downto 0);
+
+   -- Clock period definitions
+   constant Clk_period : time := 20 ns;
+ 
+BEGIN
+ 
+	-- Instantiate the Unit Under Test (UUT)
+   uut: PCS2 PORT MAP (
+          Clk => Clk,
+          Reset => Reset,
+          Result => Result
+        );
+
+   -- Clock process definitions
+   Clk_process :process
+   begin
+		Clk <= '0';
+		wait for Clk_period/2;
+		Clk <= '1';
+		wait for Clk_period/2;
+   end process;
+ 
+
+   -- Stimulus process
+   stim_proc: process
+   begin		
+      Reset <= '1';
+      wait for 40 ns;	
+	    
+		Reset <= '0';
+			
+      wait;
+   end process;
+
+END;
