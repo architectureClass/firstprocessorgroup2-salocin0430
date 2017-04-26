37640bf0eaca74c0dead666a2a9747d619ced09e
salocin0430 <nicolasruiz0430@gmail.com>
2017-04-25 22:22:28 -0500

    ADD NUEVOS OP3

--- ControlUnit.vhd
+++ ControlUnit.vhd
@@ -18,29 +18,77 @@ process(Op3,Op)
 	if(Op ="10") then
 			case Op3 is
 			when "000000" => 				-- Suma
-			Aluop <= "000001";         
+			Aluop <= "000001";     
+			
 			when "000101" => 				-- ANDN
-			Aluop <= "000010";         
+			Aluop <= "000010"; 
+			
 			when "000001" => 				-- AND
 			Aluop <= "000011"; 
+			
 			when "000010" => 				-- OR
-			Aluop <= "000100"; 
+			Aluop <= "000100";
+			
 			when "000110" => 				-- ORN
-			Aluop <= "000101"; 
+			Aluop <= "000101";
+			
 			when "000011" => 				-- XOR
 			Aluop <= "000110"; 
+			
 			when "000111" => 				-- XNOR
-			Aluop <= "000111"; 
+			Aluop <= "000111";
+			
 			when "100101" => 				-- SLL
 			Aluop <= "001000"; 
+			
 			when "100110" => 				-- SRL
 			Aluop <= "001001"; 
+			
 			when "100111" => 				-- SRA
 			Aluop <= "001010"; 
+			
 			when "000100" => 				-- SUB
-			Aluop <= "001011"; 			
+			Aluop <= "001011"; 	
+			
+			when "010000" => 				-- ADDCC
+			Aluop <= "001100"; 	
+
+			when "001000" => 				-- ADDX
+			Aluop <= "001101"; 	
+
+			when "011000" => 				-- ADDXCC
+			Aluop <= "001110"; 	
+
+			when "010100" => 				-- SUBCC
+			Aluop <= "001111"; 	
+
+			when "001100" => 				-- SUBX
+			Aluop <= "010000"; 	
+
+			when "011100" => 				-- SUBXCC
+			Aluop <= "010001"; 	
+
+			when "010001" => 				-- ANDCC
+			Aluop <= "010010"; 	
+
+			when "010010" => 				-- ORCC
+			Aluop <= "010011"; 	
+
+			when "010101" => 				-- ANDNCC
+			Aluop <= "010100"; 
+			
+			when "010110" => 				-- ORNCC
+			Aluop <= "010101";
+			
+			when "010111" => 				-- XNORCC
+			Aluop <= "010110"; 
+
+			when "010011" => 				-- XORCC
+			Aluop <= "010111"; 
+			
 			when others =>
 			Aluop <= "000000";
+			
 			end case;
 	end if;
 end process;
