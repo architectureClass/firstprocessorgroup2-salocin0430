39186d27f146bad7629c9c874c9e6630fd20830f
salocin0430 <nicolasruiz0430@gmail.com>
2017-05-02 00:05:05 -0500

    Add files via upload

--- PCS2.vhd
+++ PCS2.vhd
@@ -50,7 +50,8 @@ architecture Behavioral of PCS2 is
 	PORT(
 		E1 : IN std_logic_vector(31 downto 0);
 		E2 : IN std_logic_vector(31 downto 0);
-		Aluop : IN std_logic_vector(5 downto 0);          
+		Aluop : IN std_logic_vector(5 downto 0); 
+		C : in  STD_LOGIC;
 		Result : OUT std_logic_vector(31 downto 0)
 		);
 	END COMPONENT;
@@ -87,10 +88,9 @@ architecture Behavioral of PCS2 is
 	END COMPONENT;
 	COMPONENT RF
 	PORT(
-		Rs1 : IN std_logic_vector(4 downto 0);
-		Rs2 : IN std_logic_vector(4 downto 0);
-		Rd : IN std_logic_vector(4 downto 0);
-		Reset : IN std_logic;
+		Rs1 : IN std_logic_vector(5 downto 0);
+		Rs2 : IN std_logic_vector(5 downto 0);
+		Rd : IN std_logic_vector(5 downto 0);
 		DwR : IN std_logic_vector(31 downto 0);          
 		Crs1 : OUT std_logic_vector(31 downto 0);
 		Crs2 : OUT std_logic_vector(31 downto 0)
@@ -102,9 +102,51 @@ architecture Behavioral of PCS2 is
 		Out32 : OUT std_logic_vector(31 downto 0)
 		);
 	END COMPONENT;	
+	COMPONENT PSRModifier
+	PORT(
+		AluResult : IN std_logic_vector(31 downto 0);
+		Op1 : IN std_logic;
+		Op2 : IN std_logic;
+		Aluop : IN std_logic_vector(5 downto 0);          
+		nzvc : OUT std_logic_vector(3 downto 0)
+		);
+	END COMPONENT;	
+	
+	COMPONENT PSR
+	PORT(
+		Clk : IN std_logic;
+		nCwp : IN std_logic;
+		nzvc : IN std_logic_vector(3 downto 0);
+		Reset : IN std_logic;          
+		Cwp : OUT std_logic;
+		Carry : OUT std_logic
+		);
+	END COMPONENT;
+	COMPONENT WsMr
+	PORT(
+		Op : IN std_logic_vector(1 downto 0);
+		Op3 : IN std_logic_vector(5 downto 0);
+		Rs1 : IN std_logic_vector(4 downto 0);
+		Rs2 : IN std_logic_vector(4 downto 0);
+		Rd : IN std_logic_vector(4 downto 0);
+		Cwp : IN std_logic;          
+		nRs1 : OUT std_logic_vector(5 downto 0);
+		nRs2 : OUT std_logic_vector(5 downto 0);
+		nRd : OUT std_logic_vector(5 downto 0);
+		nCwp : OUT std_logic
+		);
+	END COMPONENT;	
 	
 	signal ResultAddInNPC ,ResNPCInPC ,PC_IM,Div_Instruc  ,ResultAlu,CRS1_ALU,CRS2_MUX, Sig_Mux,MUX_ALU: std_logic_vector(31 downto 0);
 	signal auxAluop : std_logic_vector(5 downto 0);
+	signal WRRs1 : std_logic_vector(5 downto 0);
+	signal WRRs2 : std_logic_vector(5 downto 0);
+	signal WRRd : std_logic_vector(5 downto 0);
+	signal SigCwp : std_logic;
+	signal SignCwp : std_logic;
+	signal auxCarry : std_logic;
+	signal auxnzvc : std_logic_vector(3 downto 0);
+	
 begin
 	Inst_Next_ProgramCounter: ProgramCounter PORT MAP(
 		DIn => ResultAddInNPC,
@@ -138,10 +180,9 @@ begin
 		Aluop => auxAluop
 	);
 	Inst_RF: RF PORT MAP(
-		Rs1 => Div_Instruc( 18 downto 14) ,
-		Rs2 => Div_Instruc( 4 downto 0) ,
-		Rd => Div_Instruc( 29 downto 25),
-		Reset => Reset,
+		Rs1 => WRRs1 ,
+		Rs2 => WRRs2 ,
+		Rd => WRRd,
 		DwR => ResultAlu,
 		Crs1 => CRS1_ALU ,
 		Crs2 => CRS2_MUX
@@ -165,9 +206,39 @@ begin
 	Inst_Alu: Alu PORT MAP(
 		E1 => CRS1_ALU,
 		E2 => MUX_ALU ,
+		C => auxCarry ,
 		Aluop => auxAluop ,
 		Result => ResultAlu
 	);
+
+	Inst_PSRModifier: PSRModifier PORT MAP(
+		AluResult => ResultAlu ,
+		Op1 => CRS1_ALU(31) ,
+		Op2 => MUX_ALU(31) ,
+		Aluop => auxAluop ,
+		nzvc => auxnzvc
+	);	
+	
+	Inst_PSR: PSR PORT MAP(
+		Clk => Clk,
+		nCwp => SignCwp ,
+		nzvc =>auxnzvc ,
+		Reset => Reset,
+		Cwp => SigCwp,
+		Carry => auxCarry
+	);	
+	Inst_WsMr: WsMr PORT MAP(
+		Op =>  Div_Instruc(31 downto 30) ,
+		Op3 => Div_Instruc( 24 downto 19) ,
+		Rs1 => Div_Instruc( 18 downto 14) ,
+		Rs2 => Div_Instruc( 4 downto 0),
+		Rd => Div_Instruc( 29 downto 25) ,
+		Cwp => SigCwp ,
+		nRs1 => WRRs1 ,
+		nRs2 => WRRs2 ,
+		nRd => WRRd,
+		nCwp => SignCwp
+	);	
 Result <= ResultAlu;
 
 end Behavioral;
